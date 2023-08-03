bcda3757f3652a68d95d275a8c0950c45dc8cbd6
Ghulam Qadir <connecttoqadir@gmail.com>
2023-08-03 16:04:34 +0500

    Class 1 JS

---
+++ Javascript/Class 1.js
@@ -0,0 +1,74 @@
+// Console
+
+/*
+console.log("Hello")
+console.warn("Hello")
+console.error("Hello")
+*/
+
+// Comments in JS
+// there 2 ways in js to comment your code
+// 1. Single Line Comment  ( // comment )
+// 2. Multi Line Comment   ( /* comment  */ )
+
+
+
+
+
+// Variables
+// These are name of memory locations on RAM to store data and processing results
+
+
+// There are 3 types of variables in JS
+// 1. var
+// 2. let
+// 3. const
+
+
+// Naming Rules of Variable
+// 1. Name must be started with an alphabet or Underscore _
+// 2. Space not allow
+// 3. Special Charactors like $,#,% etc are not allowed except underscore _
+// 4. Numbers can be used in variable name but not at start
+
+// Popular Naming Convention Used in Software industry
+// 1. Camel Casing    edifyCollegeOfIt
+// 2. Pascal Casing   EdifyCollegeOfIt
+
+
+
+
+const a = 1   // Declaration
+
+// const a   // Redeclare
+
+// a = 45  // Initialization
+
+// a = 23 // Overwrite
+
+// console.log(a)
+
+
+
+
+
+// Data Types in JS
+// There are 8 data types in JS
+
+
+// 1. Number        87
+// 2. String        '' "Waleed , Imran"
+// 3. Boolean       true or false
+// 4. Undefined     Declared but not assigned any value
+// 5. Null
+
+
+
+// 6. Array        [87,48,34,24,98]      
+// 7. Object {}    { name:"Waleed" , fatherName:"Imran" , contact:483838438383, isMarried: false }
+// 8. Function     function , functionName()
+
+
+
+
+
