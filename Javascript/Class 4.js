d3959d14b08e39d7d7fa47b9a3d9e8840d103df3
Ghulam Qadir <connecttoqadir@gmail.com>
2023-08-10 16:04:10 +0500

    Array Methods and Functions in js with arrow function

---
+++ Javascript/Class 4.js
@@ -0,0 +1,71 @@
+
+// Functions
+// A piece of code to perform a task
+// Functions can make our code reuseable
+
+
+
+// // simple Function
+// function sum(a,b){
+//     // console.log(a+b)
+//     return a+b
+// }
+
+// // console.log(sum(56,6))
+// // console.log(sum(7,6))
+
+
+// // Arrow Function in ES6
+// var sub = a => a*2
+// console.log(sub(5))
+
+
+
+
+
+
+// Objects
+
+
+
+// var data = {
+//     name:"Ali",
+//     age:20,
+//     4:67
+// }
+// console.log(data[age])
+
+
+
+// Array
+// Group of variables
+// var mathNums = [87,65,86,93]
+// console.log(mathNums['length'])
+
+
+
+
+// Array Menuplation
+// var mathNums = [87,65,86,93]
+
+// mathNums.unshift(98)
+// mathNums.shift()
+
+// mathNums.push(75)
+// mathNums.pop()
+
+// mathNums.splice(1,0,43,76,99)
+
+// mathNums.splice(4,0,97)
+// console.log(mathNums.slice(1,5))
+
+
+
+
+
+
+
+
+
+
+
