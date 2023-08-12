9fa98f37ea2c7fe5176c163b16423e0551bd4e83
Ghulam Qadir <connecttoqadir@gmail.com>
2023-08-12 15:57:57 +0500

    array methods map, foreach, filter, callfunctions, tempalte literals

---
+++ Javascript/Class 5.js
@@ -0,0 +1,58 @@
+// Call Back Functions
+
+
+// function a(o){
+//     o(5,6)
+//     // console.log("a")
+// }
+
+// a(function (c,d){console.log(c,d)})
+
+
+
+
+
+
+
+
+// Filter, map ( most important ) ,foreach , includes
+
+var nums = [9,6,3,4]
+
+
+// var filterNums = nums.filter(function (v){
+//     return v%2 == 0
+// })
+// console.log(filterNums)
+
+
+// var mapNums = nums.map(function(v){
+//     return v*2
+// })
+
+// console.log(mapNums)
+// console.log(nums.includes(8))
+
+
+
+
+
+
+// Template Literals
+// var f = 'Ali'
+// var l = 'Raza'
+
+// var message = `Hi,${f} ${l}`
+// console.log(message)
+
+// var t = 8
+// for(i=1;i<=10;i++){
+//     console.log(`  ${t} x ${i} = ${i*t} `)
+// }
+
+
+
+
+
+
+
